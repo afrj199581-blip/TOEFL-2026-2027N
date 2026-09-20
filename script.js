c44084ef3ca5704f3961080d3b3293390ff02705
afrj199581-blip <afrj199581@gmail.com>
2026-09-20 09:32:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const buttons = document.querySelectorAll(".container button");
 
+const links = [
+  "documents/TOEFL%20Exam%20Instructions.pdf",
+  "TOEFL%20BOOK%203.pdf",
+  "https://www.youtube.com/watch?v=2EIFMwfpL2Y",
+  "https://www.youtube.com/watch?v=0EqAtq6IDFQ",
+  "documents/TOEFL%20explain.pdf",
+  "exams/TOEFL%20Exam.html",
+  "exams/TOEFL%20interactive%20exam.html"
+];
+
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    window.open(links[index], "_blank");
+  });
+});
